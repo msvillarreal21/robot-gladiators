0e21ed439dadf9bcd43f8709d1ad17c02626a054
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-05-24 22:04:40 -0500

    update battle loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,54 +20,58 @@ var enemyAttack = 12;
 var fight = function(enemyName) {
 
   //repeat and execute as long as the enemy-robot is alive
-  while(enemyHealth > 0) {
+  while(playerHealth > 0 && enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'Skip' to choose.");
-
-    // if player choses to fight, then fight
-    if (promptFight === "FIGHT" || promptFight === "fight") {
-  
+  }
+    // if player choses to skip confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confimSkip = window.confirm("Are you sure you'd like to quit?");
+    
+    // if yes (true), leave fight
+    if (confimSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from playerMoney for skiping
+        playerMoney = playerMoney - 10;
+        console.log("playerMondy", playerMoney);
+        break;
+    }
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
-  
-    // check enemy's health
-    if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+
+      // check enemy's health
+      if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+
+    // award player money for winning
+    playerMoney = playerMoney + 20;
+
+    //leave while()  loop since enemy is dead
+    break;
+
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }   
+    // if player choses to fight, then fight
+    if (promptFight === "FIGHT" || promptFight === "fight") {
   
-    // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
+         // remove player's health by subtracting the amount set in the enemyAttack variable
+      playerHealth = playerHealth - enemyAttack;
+      console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
-  
+      );
+
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + " has died!");
+      break;
     } 
     else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-    // if player choses to skip
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-        var confimSkip = window.confirm("Are you sure you'd like to quit?");
-        
-        // if yes (true), leave fight
-        if (confimSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            //subtract money from playerMoney for skiping
-            playerMoney = playerMoney - 2;
-        }
-        //if no (falso), ask question again by running fight() again
-        else { 
-            fight();
-        }
-        //if player did not choose 1 or 2 in prompt
     
     } else {
     window.alert("You need to choose a valid option. Try again!");
@@ -77,8 +81,8 @@ var fight = function(enemyName) {
   }
   
 
-// function to start a new game
-var startGame = function() {
+    // function to start a new game
+    var startGame = function() {
     for (var i = 0; i < enemyNames.length; i++) {
       var pickedEnemyName = enemyNames[i];
       enemyHealth = 50;
@@ -86,7 +90,7 @@ var startGame = function() {
     }
     //After the loop ends, player is out of health or enemies to fight, so run the endGame function
     endGame();
-};
+      };
     // reset player stats
     playerHealth = 100;
     playerAttack = 10;
